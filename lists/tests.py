6a8d29e9ed67ddabcacf27e79209f2c15a1884b0
homeluck <526598423@qq.com>
2019-11-24 17:53:29 +0800

    精简测试代码，2个变成一个测试函数’

--- lists/tests.py
+++ lists/tests.py
@@ -5,15 +5,7 @@ from django.http import HttpRequest
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_root_url_resolve_to_home_page(self):
-        found = resolve('/')
+
+    def test_uses_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        html = response.content.decode('utf-8')
-        
-        self.assertTrue(html.startswith('<html>'))
-        self.assertTrue(html.strip().endswith('</html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
         self.assertTemplateUsed(response, 'home.html')
