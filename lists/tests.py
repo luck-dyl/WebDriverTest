56daa2c3372655f84c0d05148bae807aa3dedb69
homeluck <526598423@qq.com>
2019-11-24 17:46:20 +0800

    django 自带的 client工具代替 HttpRequest ’

--- lists/tests.py
+++ lists/tests.py
@@ -7,13 +7,13 @@ from django.http import HttpRequest
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page(self):
         found = resolve('/')
-        self.assertEqual(found.func, home_page)
-    
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
     def test_home_page_returns_correct_html(self):
-        req = HttpRequest()
-        res = home_page(req)
-        html = res.content.decode('utf-8')
-        print(repr(html))
+        response = self.client.get('/')
+        html = response.content.decode('utf-8')
+        
         self.assertTrue(html.startswith('<html>'))
         self.assertTrue(html.strip().endswith('</html>'))
         self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTemplateUsed(response, 'home.html')
