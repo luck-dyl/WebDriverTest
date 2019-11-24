66efb9afb52c1101cd81e0b877a44b69459cef7f
homeluck <526598423@qq.com>
2019-11-24 17:34:22 +0800

    注册了app , 添加了基础代码验证home.html 的应答”

--- functional_test.py
+++ functional_test.py
@@ -1,40 +1,52 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
-	
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-	
-	def tearDown(self):
-		# 退出网站，睡觉去了
-		self.browser.quit()
-	
-	def test_a_list(self):
-		#luck听说有一个非常有趣的在线编辑应用
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        # 退出网站，睡觉去了
+        self.browser.quit()
+    
+    def test_to_do_list(self):
+        #luck听说有一个非常有趣的在线编辑应用
 
-		# luck访问这个网站
-		self.browser.get('http://localhost:8000')
+        # luck访问这个网站
+        self.browser.get('http://localhost:8000')
 
-		# 他注意到了标题和头部都包含了"TO - DO"这个词
-		self.assertIn("To-Do", self.browser.title)
-		slef.fail("finish the test!")
-		
-		# 网站邀请他输入一个代办事项
+        # 他注意到了标题和头部都包含了"TO - DO"这个词
+        self.assertIn("To-Do", self.browser.title)
+        head_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', head_text)
+        
+        # 网站邀请他输入一个代办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), '请输入一个待办事项！')
+        
+        # 他在一个文本框中输入了“ 早上记忆五个英语单词”
+        inputbox.sendkeys('早上记忆五个英语单词')
+        # 他回车确认，页面刷新了数据
+        inputbox.sendkeys(Keys.Enter)
+        time.sleep(1)
+        # 显示了它输入要代办的事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1、早上记忆五个英语单词' for row in rows)
+        )
 
-		# 他在一个文本框中输入了“ 早上记忆五个单词”
+        # 页面中又显示了一个代办事项
+        self.fail("finish the test!")
 
-		# 他回车确认，页面刷新了数据
+        # 它继续添加了一条， “中午完成测试app任务”
 
-		# 显示了它输入要代办的事项
+        # 回车，页面再次刷新，显示了两项代办事项
 
-		# 页面中又显示了一个代办事项
-
-		# 它继续添加了一条， “中午完成测试app任务”
-
-		# 回车，页面再次刷新，显示了两项代办事项
-
-		# 它很满意，页面记住了它的事项
+        # 它很满意，页面记住了它的事项
 
 if __name__ == "__main__":
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-
+    
+    def test_home_page_returns_correct_html(self):
+        req = HttpRequest()
+        res = home_page(req)
+        html = res.content.decode('utf-8')
+        print(repr(html))
+        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
