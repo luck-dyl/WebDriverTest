1e4b49f4c82b78976e049cf82ad5f9d12159ccdb
homeluck <526598423@qq.com>
2019-12-01 20:07:29 +0800

    回归测试

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -29,7 +29,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
                 
-    def test_to_do_list(self):
+    def test_can_start_a_list_for_one_user(self):
         #luck听说有一个非常有趣的在线编辑应用
 
         # luck访问这个网站
@@ -52,22 +52,55 @@ class NewVisitorTest(LiveServerTestCase):
         # 显示了它输入要代办的事项
         self.wait_for_row_in_list_table('1、早上记忆五个英语单词')
 
-
         # 页面中又显示了一个代办事项
         # 它继续添加了一条， “中午完成测试app任务”
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('中午完成测试app任务')
+        inputbox.send_keys(Keys.ENTER)
         
         # 回车，页面再次刷新，显示了两项代办事项
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('1、早上记忆五个英语单词')
         self.wait_for_row_in_list_table('2、中午完成测试app任务')
-            
-
-        # 它很满意，页面记住了它的事项
-        self.fail("finish the test!")
-
-
+        
+        # 它非常高兴，睡觉去了
+        # self.fail("finish the test!")
+        
+    def test_multiple_users_can_start_lists_at_diffrent_urls(self):
+        # jack 创建了新的待办事项
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("确定一个小目标")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1、确定一个小目标')
+        # 发现生成了一个唯一的URL
+        jack_list_url = self.browser.current_url
+        self.assertRegex(jack_list_url, '/lists/.+')
+        # 现在，凯莉的新用户访问了该网站
+        ## 我们使用了一个新的用户浏览器会话
+        ## 确保凯莉的信息不会从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        # 凯莉访问了首页
+        self.browser.get(self.live_server_url)
+        # 页面中看不jack的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('早上记忆五个英语单词', page_text)
+        self.assertNotIn('确定一个小目标', page_text)
+        
+        # 凯莉添加了一个待办事项
+        inputbox = self.webdriver.find_element_by_id('id_new_item')
+        inputbox.send_keys('凯莉买衣服')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('凯莉买衣服')
+        # 凯莉也获得她唯一的url
+        caryly_list_url = self.browser.current_url
+        self.assertRegex(caryly_list_url, '/list/+.')
+        # 这个页面还是没有jack的待办清单
+        self.wait_for_row_in_list_table('早上记忆五个英语单词')
+        self.wait_for_row_in_list_table('确定一个小目标')
+        # 两人都非常满意，睡觉去了
+        self.browser.quit()
+        
 
 # if __name__ == "__main__":
     # unittest.main(warnings='ignore')
