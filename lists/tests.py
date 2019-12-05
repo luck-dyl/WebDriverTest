5247355b2775218481644d267f1d9937f624f493
homeluck <526598423@qq.com>
2019-12-05 23:00:34 +0800

    增加 models Item 的 外键关联 List

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -10,15 +10,23 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
         
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = "计划买个棉被"
+        first_item.list = list_
         first_item.save()    
         
         second_item = Item()
         second_item.text = "计划买个锤子"
+        second_item.list = list_
         second_item.save()
+        
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -26,7 +34,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "计划买个棉被")
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, "计划买个锤子")
+        self.assertEqual(second_saved_item.list, list_, )
         
 class ListViewTest(TestCase):
 
