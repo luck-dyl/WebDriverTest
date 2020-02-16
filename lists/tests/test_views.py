c22ca0ac2601403ef2ee0ede9ffb0747ae078c69
homeluck <526598423@qq.com>
2020-02-16 12:17:15 +0800

    调整新增待办事项，为空的处理

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.utils.html import escape
+
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -37,6 +39,8 @@ class ListViewTest(TestCase):
         self.assertEqual(response.context['list'], correct_list)
 
 
+
+
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
@@ -58,3 +62,17 @@ class NewListTest(TestCase):
         response = self.client.post(f'/lists/{correct_list.id}/add_item',
                                     data={'item_text': 'a new list item for an existing list'})
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        # escape 可以处理 can't 中 的“’”被转义的问题
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
