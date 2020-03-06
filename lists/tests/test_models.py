fd96b7cead7d8d396b0d20ad7430940038ca23c7
homeluck <526598423@qq.com>
2020-03-06 21:55:11 +0800

    Implement duplicate item validation at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -3,35 +3,21 @@ from lists.models import Item, List
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
 
+
 # Create your tests here.
 
 class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
 
-        first_item = Item()
-        first_item.text = "计划买个棉被"
-        first_item.list = list_
-        first_item.save()
+    def test_item_isrelated_to_list(self):
+        list_ = List.objects.create()
+        item =Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
 
-        second_item = Item()
-        second_item.text = "计划买个锤子"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "计划买个棉被")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "计划买个锤子")
-        self.assertEqual(second_saved_item.list, list_, )
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -44,3 +30,16 @@ class ListAndItemModelsTest(TestCase):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
 
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean()  # 不该抛出异常
