2e20a866eecfc5347df4ad57d2e546557efa0862
homeluck <526598423@qq.com>
2020-04-14 17:39:11 +0800

    FT for login via email

---
+++ functional_tests/test_login.py
@@ -0,0 +1,35 @@
+from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
+from django.core import mail
+import re
+
+TEST_EMAIL = "526598423@qq.com"
+SUBJECT = "Your login link for Superlists"
+
+
+class LoginTest(FunctionalTest):
+
+    def test_can_get_emial_link_to_login(self):
+        # 访问网站
+        self.browser.get(self.live_server_url)
+        # 输入邮箱账号，回车
+        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertIn("Check your email", self.browser.find_element_by_tag_name('body').text))
+
+        # 查看邮件，验证关键内容
+        email = mail.outbox[0]
+        self.assertIn(TEST_EMAIL, email.to)
+        self.assertEqual(SUBJECT, email.subject)
+        url_search = re.search(r'http://.+/.+$', email.body)
+        if not url_search:
+            self.fail(f'Could not find url in email body: \n{email.body}')
+        url = url_search.group(0)
+        self.assertIn(self.live_server_url, url)
+
+        # 访问超链接，验证登录页面
+        self.browser.get(url)
+        self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(TEST_EMAIL, navbar.text)
